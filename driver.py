50b1ee488563ab170637f9e0d2da7ff5d1330054
Nick Smith <nick.smith@cern.ch>
2019-05-30 10:54:44 -0500

    Job splitting

--- driver.py
+++ driver.py
@@ -2,11 +2,36 @@
 import argparse
 import json
 import warnings
+import math
 
 import ROOT
 ROOT.gROOT.SetBatch(True)
 ROOT.PyConfig.IgnoreCommandLineOptions = True
 ROOT.PyConfig.DisableRootLogon = True
+
+
+def setup_samplelist(args):
+    with open(args.sample) as fin:
+        samples_in = json.load(fin)
+
+    if args.split is not None:
+        # Split the filelist into even chunks
+        njob, ijob = args.split
+        all_files = sum((info['files'] for info in samples_in.values()), [])
+        files_per_job = math.ceil(len(all_files) / njob)  # ! Assumes python 3 true division
+        files_this_job = all_files[ijob*files_per_job:(ijob+1)*files_per_job]
+        samples = {}
+        for sample, info in samples_in.items():
+            files = [f for f in info.pop('files') if f in files_this_job]
+            if len(files) == 0:
+                continue
+            info['files'] = files
+            samples[sample] = info
+    else:
+        samples = samples_in
+
+    print(samples)
+    return samples
 
 
 def setup_selector(args, selector, dataset, info):
@@ -43,11 +68,9 @@ def run(args):
     warnings.filterwarnings(action='ignore', category=RuntimeWarning, message='no dictionary for class')
     SelectorType = getattr(ROOT, args.selector)
 
-    with open('datadef_nano.json') as fin:
-        samples = json.load(fin)
-
     outputFile = ROOT.TFile(args.output, "recreate")
 
+    samples = setup_samplelist(args)
     for dataset, info in samples.items():
         print(f"Processing {dataset}")
         filelist = info['files']
@@ -77,7 +100,9 @@ if __name__ == '__main__':
     parser.add_argument('--selector', default='NanoSelector', help='The name of the selector to use (default: %(default)s)')
     parser.add_argument('--limit', type=int, default=None, metavar='N', help='Limit to the first N files of each dataset in sample JSON')
     parser.add_argument('--maxevents', type=int, default=None, metavar='N', help='Limit to the first N entries of the tree in each file')
+    parser.add_argument('--sample', default='datadef_nano.json', help='Input sample list json (default: %(default)s)')
     parser.add_argument('--output', default='output.root', help='Output filename (default: %(default)s)')
+    parser.add_argument('--split', type=int, help="Split input file list and process subsection. IJOB is zero-indexed", nargs=2, metavar=('NJOBS', 'IJOB'))
     args = parser.parse_args()
 
     run(args)
